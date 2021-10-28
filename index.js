88a0dd3acb4b00d28bca6e49ebaff90e837383f6
zhuw2021 <81784365+zhuw2021@users.noreply.github.com>
2021-10-28 17:27:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 const port = 3000
 
 app.get("/", (req, res) => {
-  res.send("Hello World! - Nodejs");
-});
+  res.send("Hello World! - Nodejs")
+})
+
 app.listen(port, () => {
   console.log(`Example app listnening at http://localhost:${port}`)
 })
